e4a007e9e1e7e4bc611ffa9b403d931fc51f365f
Thomas Demmer <tdemmer18@gmail.com>
2019-02-22 14:14:11 -0500

    Updated for 2019-02-22 Games

--- nhl_add_dates_to_nhl_schedule_boxscores.R
+++ nhl_add_dates_to_nhl_schedule_boxscores.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 
 source("~/rstats/hockey/nhl_api/nhl_schedule_20182019.R")
-all_games_2019_02_21 <- read_csv("~/rstats/hockey/nhl_api/csv/all_games_through_2019-02-21.csv")
+all_games_2019_02_22 <- read_csv("~/rstats/hockey/nhl_api/csv/all_games_through_2019-02-22.csv")
 
 nhl_20182019_game_schedule <- nhl_20182019_game_schedule %>%
   rename(game_id = gamePk)
 
 nhl_20182019_game_schedule
 
-all_games_with_dates <- all_games_2019_02_21 %>%
+all_games_with_dates <- all_games_2019_02_22 %>%
   left_join(nhl_20182019_game_schedule, by = "game_id")
 
 write_csv(all_games_with_dates, "~/rstats/hockey/nhl_api/csv/all_games_with_dates.csv")
+
+View(all_games_with_dates)
