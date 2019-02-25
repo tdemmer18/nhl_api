5c5e69a68de655efcc4b89b99e78062049accfa2
Thomas Demmer <tdemmer18@gmail.com>
2019-02-25 11:57:12 -0500

    Added games through 2019-02-24

--- nhl_api_boxScore.R
+++ nhl_api_boxScore.R
@@ -60,7 +60,7 @@ nhlBoxScore <- function(game_id) {
 
 
 ########## USE THIS TO DOWNLOAD GAMES FOR A RANGE OF GAMES ##########
-game_range <- 2018020891:2018020939
+game_range <- 2018020940:2018020961
 all_games_20182019 <- lapply(game_range, nhlBoxScore)
 #View(all_games_20182019)
 
